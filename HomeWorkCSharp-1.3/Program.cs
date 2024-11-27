15b90e1df5947156addd778d27c8809dae617e4f
Владислав Васин <VasinVladislav94@gmail.com>
2024-11-27 11:14:16 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkCSharp-1.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCSharp_1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.3.Составить программу вывода на экран числа, вводимого с клавиатуры. Выводимому числу должно предшествовать сообщение «Вы ввели число».            
+            int value = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Вы ввели число " + value);
+        }
+    }
+}
